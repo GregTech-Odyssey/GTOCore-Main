80f873f681c61163d9775c04c00995f9874fb45e
nutant <65939836+nutant233@users.noreply.github.com>
2026-07-02 22:58:56 +0800

    戴森球加一个0.85的超频

--- src/main/java/com/gtocore/common/machine/tesseract/TesseractDirectedTarget.java
+++ src/main/java/com/gtocore/common/machine/tesseract/TesseractDirectedTarget.java
@@ -1,10 +1,10 @@
 package com.gtocore.common.machine.tesseract;
 
-import com.gto.datasynclib.datasream.codec.ByteStreamCodec;
 import net.minecraft.core.Direction;
 import net.minecraft.core.GlobalPos;
 import net.minecraft.network.FriendlyByteBuf;
 
+import com.gto.datasynclib.datasream.codec.ByteStreamCodec;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import org.jetbrains.annotations.NotNull;
@@ -17,7 +17,7 @@ public record TesseractDirectedTarget(GlobalPos pos, Direction face, int order) 
             GlobalPos.CODEC.fieldOf("pos").forGetter(TesseractDirectedTarget::pos),
             Direction.CODEC.fieldOf("face").forGetter(TesseractDirectedTarget::face),
             Codec.INT.fieldOf("order").forGetter(TesseractDirectedTarget::order)).apply(i, TesseractDirectedTarget::new));
-    public static final ByteStreamCodec<TesseractDirectedTarget> STREAM_CODEC =ByteStreamCodec.of(TesseractDirectedTarget::writeToBuffer,TesseractDirectedTarget::readFromBuffer);
+    public static final ByteStreamCodec<TesseractDirectedTarget> STREAM_CODEC = ByteStreamCodec.of(TesseractDirectedTarget::writeToBuffer, TesseractDirectedTarget::readFromBuffer);
     public static final Comparator<TesseractDirectedTarget> SORTER = Comparator
             .comparingInt((TesseractDirectedTarget pf) -> {
                 if (pf.order() < 0) {
