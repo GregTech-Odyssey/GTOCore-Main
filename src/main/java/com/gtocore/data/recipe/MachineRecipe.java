e9fa999818811b9a82ec23ce03e178066d8071c2
mutan <1040179812@qq.com>
2026-09-25 22:17:42 +0800

    fix: ME 磁盘箱子配方保留 MV 电路（PDP/CHC/PPP）

--- src/main/java/com/gtocore/data/recipe/MachineRecipe.java
+++ src/main/java/com/gtocore/data/recipe/MachineRecipe.java
@@ -56,7 +56,7 @@ public final class MachineRecipe {
                 "CAC", "PCP", 'P', new MaterialEntry(plateDouble, GTMaterials.StainlessSteel), 'C', CustomTags.HV_CIRCUITS,
                 'A', AEItems.CELL_COMPONENT_1K.asItem());
         VanillaRecipeHelper.addShapedRecipe(true, GTOCore.id("me_disk_box"), GTAEMachines.ME_DISK_BOX.asItem(), "PDP",
-                "PHP", "PPP", 'P', new MaterialEntry(plateDouble, GTMaterials.Aluminium), 'D', AEBlocks.DRIVE.block().asItem(),
+                "CHC", "PPP", 'P', new MaterialEntry(plateDouble, GTMaterials.Aluminium), 'C', CustomTags.MV_CIRCUITS, 'D', AEBlocks.DRIVE.block().asItem(),
                 'H', GTMachines.HULL[MV].asItem());
         VanillaRecipeHelper.addShapedRecipe(true, GTOCore.id("evaporation_plant"),
                 MultiBlockA.EVAPORATION_PLANT.asItem(), "CBC", "FMF", "CBC", 'M', GTMachines.HULL[HV].asItem(),
